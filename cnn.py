corrected transformer weights calculation and some other stuff

cnn.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-input_shape_cnn = (1, 3, 100, 100)              #batch_size, num_channels, height, width
+input_shape_cnn = (1, 6, 12, 3)              #batch_size, num_channels, height, width
 
 class DepthwiseSeparableConv(nn.Module):
     def __init__(self, in_channels = 3, out_channels = 1, kernel_size = 3, stride = 1, padding = 'valid'):
@@ -23,37 +23,37 @@ class CNN(nn.Module):
         super(CNN, self).__init__()
         # self.dwsp_conv = nn.Conv3d()#DepthwiseSeparableConv(in_channels = 3, out_channels = 128, kernel_size = 3, stride = (1, 1), padding = 'valid')
         # self.act0 = nn.ReLU()
-        # self.conv1 = nn.Conv2d(in_channels = 128, out_channels = 32, kernel_size = 3, stride = (1, 1), padding = 'valid')
+        self.conv1 = nn.Conv2d(in_channels = 6, out_channels = 10, kernel_size = 3, stride = (1, 1), padding = 1)
         # self.act1 = nn.ReLU()
-        # self.pool1 = nn.MaxPool2d(kernel_size = (2, 2), stride = (2, 2))
-        # self.conv2 = nn.Conv2d(in_channels = 32, out_channels = 64, kernel_size = 3, stride = (1, 1), padding = 'valid')
+        self.pool1 = nn.MaxPool2d(kernel_size = (3, 1))
+        self.conv2 = nn.Conv2d(in_channels = 10, out_channels = 15, kernel_size = 5, stride = (1, 1), padding = 2)
         # self.act2 = nn.ReLU()
-        # self.pool2 = nn.MaxPool2d(kernel_size = (2, 2), stride = (2, 2))
+        self.pool2 = nn.MaxPool2d(kernel_size = (4, 3))
         # self.flatten = nn.Flatten()
         # self.fc1 = nn.Linear(2304, 32)
         # self.fc2 = nn.Linear(32, 64)
         # self.fc3 = nn.Linear(64, 10)
         # self.act_out = nn.Softmax(dim=1)
-        self.conv1 = nn.Conv2d(in_channels = 3, out_channels = 3, kernel_size = 3, stride = 1, padding = 0, dilation = 2)
-        self.conv2 = nn.Conv2d(in_channels = 3, out_channels = 3, kernel_size = 3, stride = 1, padding = 0, dilation = 2)
+        # self.conv1 = nn.Conv2d(in_channels = 3, out_channels = 3, kernel_size = 3, stride = 1, padding = 0, dilation = 1)
+        # self.conv2 = nn.Conv2d(in_channels = 3, out_channels = 3, kernel_size = 3, stride = 1, padding = 0, dilation = 1)
         # self.conv3 = nn.Conv2d(in_channels = 3, out_channels = 5, kernel_size = 3, stride = 1, padding = 'valid')
         # self.conv4 = nn.Conv2d(in_channels = 3, out_channels = 3, kernel_size = 3, stride = 1, padding = 1)
         # self.conv5 = nn.Conv2d(in_channels = 3, out_channels = 3, kernel_size = 3, stride = 2, padding = 1)
     def forward(self, x):
         # x = self.dwsp_conv(x)
         # x = self.act0(x)
-        # x = self.conv1(x)
+        x = self.conv1(x)
         # x = self.act1(x)
-        # x = self.pool1(x)
-        # x = self.conv2(x)
+        x = self.pool1(x)
+        x = self.conv2(x)
         # x = self.act2(x)
-        # x = self.pool2(x)
+        x = self.pool2(x)
         # x = self.flatten(x)
         # x = self.fc1(x)
         # x = self.fc2(x)
         # x = self.fc3(x)
 
-        x = self.conv1(x)
-        x = self.conv2(x)
+        # x = self.conv1(x)
+        # x = self.conv2(x)
 
         return x

rnn.py
@@ -3,14 +3,14 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-input_shape_rnn = (4, 256)         #(timesteps or sequence length, input features)
+input_shape_rnn = (3, 120)         #(timesteps or sequence length, input features)
 
 class RNNModel(nn.Module):
     def __init__(self, input_shape=input_shape_rnn):
         super(RNNModel, self).__init__()
-        self.lstm1 = nn.RNN(input_size = 256, hidden_size = 512, num_layers = 1, nonlinearity = 'tanh', bias = False, bidirectional = False)
+        self.rnn1 = nn.RNN(input_size = 120, hidden_size = 300, num_layers = 1, nonlinearity = 'tanh', bias = True, bidirectional = False)
         
     def forward(self, x):
-        x = self.lstm1(x)
+        x = self.rnn1(x)
 
         return x

transformer.py
@@ -216,7 +216,7 @@ class TransformerModel(nn.Module):
         print("Encoder-Decoder Attention Weights dimensions softmax((Q * K.t)/√dk)): (num_heads, batch_size, output sequence length, input sequence length)", attention.shape)
         print("Encoder-Decoder Attention Weights matrix (for a single head) number of elements: (max_in_len * max_out_len)", torch.numel(attention)/num_heads)
         print("Number of output units of the final fully connected layer = number of features in a decoder output vector = output vocab size: ", decoding.shape[-1])
-        print("Total number of weights in a single multi-headed self attention module: (embed_dims * embed_dims*3)", embed_dim*embed_dim*3)
+        print("Total number of weights in a single multi-headed self attention module: For all Q, K and V combined: (embed_dims * embed_dims*3) and for W_O: (embed_dims*embed_dims)", embed_dim*embed_dim*4)
 
         return decoding, attention
 